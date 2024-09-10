8744df320d1413f1886ea225892921b3f786455b
roshanleharwani <roshanleharwani@gmail.com>
2024-09-10 19:30:13 +0530

    Route updated

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const app = express()
 const controller = require('./controllers/appController')
 const axios = require('axios')
 const { Groq } = require('groq-sdk'); 
-const { name } = require('ejs')
 
 
 app.set('view engine','ejs')
@@ -18,82 +17,9 @@ app.get('/',(req,res)=>{
 })
 
 
-app.get('/report', async (req, res) => {
-  const barcode = 	5449000000996;
-  const uri = `https://world.openfoodfacts.org/api/v1/product/${barcode}.json`;
+app.get('/report/:barcode',controller.report )
 
-  async function getProductData() {
-    try {
-      const response = await axios.get(uri);
-      const data = response.data;
-
-      const groq = new Groq({
-        // apiKey: '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',
-        apiKey: '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',
-        
-      });
-
-      const template = `{
-          "productCategory": [2-3 categories],
-          "productInterestingFacts": [2-3 each fact of min 10 words],
-          "scoresOutOf100": {
-            "healthScore": score,
-            "intermediateRiskScore": score,
-            "longTermRiskScore": score
-          },
-          "ingredients": [
-            {
-              "ingredient1": {
-                "description": "briefDescription of min 10 words",
-                "effects": [2-3 harmful effects]
-              }
-            }
-          ],
-          "healthRecommendations": [3-4 health recommendations],
-          "betterProductAlternatives": [3-4 alternatives with respect to Indian region 
-            {
-              "alternativeName": {
-                "briefBenefit": "briefBenefitDescription"
-              }
-            }
-          ]
-        }`;
-
-      const productName = data['product']['product_name'];
-      const prompt = `You are a Nutrition expert. Provide a JSON response only, following this format: ${template}. Include ingredients with health impacts, excluding common ones like salt or water. If ingredients are encoded, give their full names. Use the product name ${productName} and ingredients from ${data['product']['ingredients_text']}.`
-
-      const chatCompletion = await groq.chat.completions.create({
-        messages: [
-          {
-            role: 'user',
-            content: prompt,
-          },
-        ],
-        model: 'llama-3.1-70b-versatile',
-        max_tokens: 8000,
-      });
-
-      const jsonObj = JSON.parse(chatCompletion.choices[0]?.message?.content || '{}');
-      return { name: productName, data: jsonObj };
-    } catch (error) {
-      console.error('Error fetching data or completing chat:', error);
-      return null;
-    }
-  }
-
-  
-  const data = await getProductData(); // Add 'await' here
-
-  
-  if (!data) {
-    return res.status(500).render('error');
-  }
-  const references = await controller.scraper(data.name + 'health Impacts')
-
-  res.render('report', { data: data,references:references }); // Now you have 'data' properly fetched
-});
-
-app.get('/reportGenerate',controller.generate)
+app.get('/reportGenerate/:barcode',controller.generate)
 
 app.listen(3000)
 
--- controllers/appController.js
+++ controllers/appController.js
@@ -3,15 +3,15 @@ const puppeteer = require('puppeteer')
 const path = require('path')
 
 exports.generate = async (req, res) => {
+    const barcode = req.params.barcode;
     let browser;
     try {
-        // Launch browser explicitly in headless mode with a timeout
         browser = await puppeteer.launch();
 
         const page = await browser.newPage();
 
         // Set a timeout for navigation
-        await page.goto(`${req.protocol}://${req.get('host')}/report`, {
+        await page.goto(`${req.protocol}://${req.get('host')}/report/barcode`, {
             waitUntil: 'networkidle2',
         });
 
@@ -48,7 +48,7 @@ exports.generate = async (req, res) => {
 };
 
 
-exports.scraper = async (topic)=> {
+scraper = async (topic)=> {
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
   
@@ -86,9 +86,79 @@ exports.scraper = async (topic)=> {
   
     return searchResults;
   }
+
+  exports.report = async (req, res) => {
+    const barcode = 	req.params.barcode;
+    const uri = `https://world.openfoodfacts.org/api/v1/product/${barcode}.json`;
   
-  // Usage example
-//   fetchTopGoogleSearchResults('bru gold health impacts ').then(searchResults => {
-//     console.log(searchResults);
-//   });
+    async function getProductData() {
+      try {
+        const response = await axios.get(uri);
+        const data = response.data;
   
+        const groq = new Groq({
+          // apiKey: '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',
+          apiKey: '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',
+          
+        });
+  
+        const template = `{
+            "productCategory": [2-3 categories],
+            "productInterestingFacts": [2-3 each fact of min 10 words],
+            "scoresOutOf100": {
+              "healthScore": score,
+              "intermediateRiskScore": score,
+              "longTermRiskScore": score
+            },
+            "ingredients": [
+              {
+                "ingredient1": {
+                  "description": "briefDescription of min 10 words",
+                  "effects": [2-3 harmful effects]
+                }
+              }
+            ],
+            "healthRecommendations": [3-4 health recommendations],
+            "betterProductAlternatives": [3-4 alternatives with respect to indian perspective  of same category
+              {
+                "alternativeName": {
+                  "briefBenefit": "briefBenefitDescription"
+                }
+              }
+            ]
+          }`;
+  
+        const productName = data['product']['product_name'];
+        const prompt = `You are a Nutrition expert. Provide a JSON response only, following this format: ${template}. Include all ingredients with health impacts. If ingredients are encoded, give their full names. Use the product name ${productName} and ingredients from ${data['product']['ingredients_text']}.`
+        // console.log(prompt)
+        const chatCompletion = await groq.chat.completions.create({
+          messages: [
+            {
+              role: 'user',
+              content: prompt,
+            },
+          ],
+          model: 'llama-3.1-70b-versatile',
+          max_tokens: 8000,
+          temperature:0.5
+        });
+  
+        const jsonObj = JSON.parse(chatCompletion.choices[0]?.message?.content || '{}');
+        return { name: productName, data: jsonObj };
+      } catch (error) {
+        console.error('Error fetching data or completing chat:', error);
+        return null;
+      }
+    }
+  
+    
+    const data = await getProductData(); // Add 'await' here
+  
+    
+    if (!data) {
+      return res.status(500).render('error',{barcode:barcode});
+    }
+    const references = await scraper(data.name + 'health Impacts')
+  
+    res.render('report', { data: data,references:references });
+  }
